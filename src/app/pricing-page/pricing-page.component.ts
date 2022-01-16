820ab86c9af8cd5af6be432bcea81e3735b2d7b2
Daniel Kovacs <info@kovix.info>
2022-01-16 20:04:43 +0100

    pricing-page ready, fixed missing param in app.component.html

--- src/app/pricing-page/pricing-page.component.ts
+++ src/app/pricing-page/pricing-page.component.ts
@@ -14,7 +14,44 @@ interface IPriceItem {
 })
 export class PricingPageComponent implements OnInit {
 
-  priceTable: IPriceItem[] = [];
+  priceTable: IPriceItem[] = [
+    {
+      name: 'Free', 
+      price: 0,
+      options: [
+        '10 tanuló',
+        '2GB tárhely',
+        'Email támogatás',
+        'Dokumentáció'
+      ],
+      btnText: 'Ingyenes regisztráció' 
+    },
+    {
+      name: 'Pro', 
+      price: 15,
+      options: [
+        '20 tanuló',
+        '10GB tárhely',
+        'Kiemelt email támogatás',
+        'Dokumentáció'
+      ],
+      btnText: 'Kezdő lépések' 
+    },
+    {
+      name: 'Enterprise', 
+      price: 29,
+      options: [
+        '100 tanuló',
+        '100 GB tárhely',
+        'Telefonos és email támogatás',
+        'Dokumentáció'
+      ],
+      btnText: 'Kontakt' 
+    },
+
+
+    
+  ];
 
   constructor() { }
 
